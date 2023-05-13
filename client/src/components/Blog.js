673a490bd06335a0dcb4905e670a495842d53353
karthik akarapu <akarapukarthik43@gmail.com>
2023-05-13 14:27:32 +0530

    first commit

---
+++ client/src/components/Blog.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import sound from "./sound.mp3"
+import { useNavigate } from "react-router-dom";
+import {
+  Card,
+  Avatar,
+  CardContent,
+  CardHeader,
+  Typography,
+  CardMedia,
+  Box,
+  IconButton,
+} from "@mui/material";
+import {
+  DeleteForeverOutlined,
+  FunctionsOutlined,
+  ModeEditOutlineOutlined,
+} from "@mui/icons-material";
+import axios from "axios";
+
+const Blog = ({ title, content, image, userName, isUser, id }) => {
+
+  const [music, setMusic] = useState(null)
+
+  const navigate = useNavigate();
+  
+  const handleEdit = (event) => {
+    navigate(`/myBlogs/${id}`);
+  };
+
+  const deleteRequest = async () => {
+    const res = await axios
+      .delete(`http://localhost:8000/api/blog/${id}`)
+      .catch((err) => console.log(err));
+
+    const data = res.data;
+    return data;
+  };
+
+  const handleDelete = () => {
+    deleteRequest().then(() => navigate("/"));
+  };
+  function playMusic(){
+    var audio = new Audio(sound);
+    audio.play()
+    setMusic(audio)
+  }
+
+  function stopMusic(){
+    music.pause();
+  }
+
+  return (
+    <div >
+      <Card
+        sx={{
+          width: "50%",
+          margin: "auto",
+          marginTop: 2,
+          padding: 2,
+          boxShadow: "5px 5px 10px #ccc",
+          ":hover": { boxShadow: "10px 10px 20px #ccc" },
+        }}
+      >
+        {isUser && (
+          <Box display={"flex"}>
+            <IconButton onClick={handleEdit} sx={{ marginLeft: "auto" }}>
+              <ModeEditOutlineOutlined color="info" />
+            </IconButton>
+            <IconButton onClick={handleDelete}>
+              <DeleteForeverOutlined color="error" />
+            </IconButton>
+          </Box>
+        )}
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+              {userName && userName.charAt(0)}
+            </Avatar>
+          }
+          title={title}
+          subheader=""
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={image}
+          alt="Paella dish"
+        />
+        <CardContent>
+          <hr />
+          <br />
+          <Typography variant="body2" color="text.secondary">
+            <b>{userName}</b> {": "}
+            {content}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ client/src/components/Header.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+
+  const navigate = useNavigate()
+
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  const [value, setValue] = useState();
+  return (
+    <AppBar
+      position="sticky"
+      sx={{
+        background:
+          "linear-gradient(90deg, rgba(240,13,205,1) 0%, rgba(0,212,255,1) 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography
+          component={Link}
+          to="/"
+          variant="h4"
+          style={{ textDecoration: "none" }}
+        >
+          Teching
+        </Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"}>
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(event, val) => setValue(val)}
+            >
+              <Tab LinkComponent={Link} to="/" label="All articles" />
+              <Tab LinkComponent={Link} to="/myBlogs" label="My Articles " />
+              <Tab LinkComponent={Link} to="/blogs/add" label="Create Articles " />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+              >
+                Sign In
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+              >
+                Sign Up
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Button
+              onClick={() => dispatch(authActions.logout())}
+              LinkComponent={Link}
+              to="/"
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+            >
+              Log Out
+            </Button>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
